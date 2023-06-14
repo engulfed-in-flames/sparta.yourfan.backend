5c7f85e72e89c0d7590c8e63449f1a7a51af99a7
engulfedInFlames <recona97@gmail.com>
2023-06-14 09:11:22 +0900

    1차 프로젝트 병합 #16

--- manage.py
+++ manage.py
@@ -2,9 +2,10 @@
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shortcut.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yourfan.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
