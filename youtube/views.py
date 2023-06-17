fccdb7a91812a6fe51270986dae9fbe4a682c9f9
raoneli1013 <114127147+raoneli1013@users.noreply.github.com>
2023-06-17 13:31:17 +0900

    find 오류 수정

--- youtube/views.py
+++ youtube/views.py
@@ -34,29 +34,29 @@ class ChannelModelView(APIView):
     
     def post(self, request, channel_id):
         youtube = youtube_api.youtube
-        if Channel.objects.get(channel_id=channel_id):
-            return Response({'message':'이미 존재하는 채널입니다'})
-        channel_data = youtube_api.get_channel_stat(youtube, channel_id)
-        if channel_data=='error':
-            return Response({'message':'존재하지 않는 채널입니다'})
-        topic = youtube_api.topic_id_dict
-        channel_data['channel_id']=channel_id
-        channel_data['topic_id']=[]
-        if channel_data['topic_ids']:
-            for i in channel_data['topic_ids']:
-                channel_data['topic_id'].append(topic[i])
+        try :
+            if Channel.objects.get(channel_id=channel_id):
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        except:
+            channel_data = youtube_api.get_channel_stat(youtube, channel_id)
 
-        serializer = serializers.CreateChannelSerializer(data=channel_data)
-        if serializer.is_valid():
-            channel=serializer.save()
-            detail_serializer = serializers.CreateChannelDetailSerializer(data=channel_data)
-            if detail_serializer.is_valid():
-                detail_serializer.save(channel=channel)
-                return Response({'channel_data':serializer.data,'channel_detail_data':detail_serializer.data}, status=status.HTTP_201_CREATED)
+            topic = youtube_api.topic_id_dict
+            channel_data['topic_id']=[]
+            if channel_data['topic_ids']:
+                for i in channel_data['topic_ids']:
+                    channel_data['topic_id'].append(topic[i])
+
+            serializer = serializers.CreateChannelSerializer(data=channel_data)
+            if serializer.is_valid():
+                channel=serializer.save()
+                detail_serializer = serializers.CreateChannelDetailSerializer(data=channel_data)
+                if detail_serializer.is_valid():
+                    detail_serializer.save(channel=channel)
+                    return Response({'channel_data':serializer.data,'channel_detail_data':detail_serializer.data}, status=status.HTTP_201_CREATED)
+                else:
+                    return Response({'channel_detail_error':detail_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
             else:
-                return Response({'channel_detail_error':detail_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response({'channel_error':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({'channel_error':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, channel_id):
         channel = Channel.objects.get(channel_id=channel_id)
@@ -78,7 +78,7 @@ class ChannelModelView(APIView):
     def delete(self, request, channel_id):
         channel = get_object_or_404(Channel,channel_id=channel_id)
         channel.delete()
-        return Response({'message':'채널 삭제 완료'}, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_200_OK)
 
 
 class ChannelDetailView(APIView):
