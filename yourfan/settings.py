536e3a24fa23654466d147bfc98ce10207d2b0f6
brighto <jychoi1996@gmail.com>
2023-06-19 19:59:42 +0900

    fix settings

--- yourfan/settings.py
+++ yourfan/settings.py
@@ -222,8 +222,8 @@ SOCIALACCOUNT_LOGIN_ON_GET = True
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = str(os.environ.get("EMAIL_HOST_USER"))
-EMAIL_HOST_PASSWORD = str(os.environ.get("EMAIL_HOST_PASSWORD"))
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
