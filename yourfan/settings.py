0716f255cefdc3e7234aa57f35b169753fda109e
engulfedInFlames <recona97@gmail.com>
2023-06-30 18:06:00 +0900

    update: settings.py 환경 변수 관련 코드 일부 수정

--- yourfan/settings.py
+++ yourfan/settings.py
@@ -1,5 +1,12 @@
 import os
 from datetime import timedelta
+from pathlib import Path
+
+# from dotenv import read_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+# ENV_FILE_PATH = BASE_DIR / ".env"
+# read_dotenv(str(ENV_FILE_PATH))
 
 SECRET_KEY = str(os.environ.get("DJANGO_SECRET_KEY"))
 
