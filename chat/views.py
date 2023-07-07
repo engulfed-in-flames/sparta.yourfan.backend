328cafc2503e58d064366b796a40f43bc6bbcf32
scarlet0star <scarlet0star@gmail.com>
2023-07-07 15:02:48 +0900

    fix: 스태프 수정 이슈 해결 & chat 입장시 check api 생성

--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,38 @@
-from rest_framework import viewsets
-from .models import Chatroom,Message
-from .serializers import ChatroomSerializer,MessageSerializer
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from .models import Chatroom, Message
+from .serializers import ChatroomSerializer, MessageSerializer
+
 # Create your views here.
 
+
 class ChatroomViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Chatroom.objects.all()
+    queryset = Chatroom.objects.all().order_by("-created_at")
     serializer_class = ChatroomSerializer
-    
+    lookup_field = "board__custom_url"
+
+    @action(detail=True, methods=["GET"])
+    def check(self, request, board__custom_url=None):
+        chatroom = self.get_object()
+        user = request.user
+
+        is_duplicate = chatroom.user.filter(pk=user.pk).exists()
+        is_banned_user = chatroom.board.banned_users.filter(pk=user.pk).exists()
+
+        if is_duplicate:
+            return Response(
+                {"message": "Duplicated user"}, status=status.HTTP_401_UNAUTHORIZED
+            )
+        elif is_banned_user:
+            return Response(
+                {"message": "Banned user"}, status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        return Response(status=status.HTTP_200_OK)
+
+
 class MessageViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
